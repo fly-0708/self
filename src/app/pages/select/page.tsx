e9394db511979b54324745b0ff64ef9419addaea
fly <1833174181@qq.com>
2023-12-26 10:37:30 +0800

    yarn select

---
+++ src/app/pages/select/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react';
+import {Box} from '@mui/material'
+
+const Select = () => {
+    return (
+        <Box>
+            select
+        </Box>
+    );
+};
+
+export default Select;
